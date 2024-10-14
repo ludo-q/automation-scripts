cdf0da6af34cadf93bb4d9c0e924fa6b7ddf5920
Ludvig Qerraxhija <l.qerraxhija@gmail.com>
2024-10-14 12:37:43 +0200

    Fixed script sync-git-docker-ignore.sh

--- scripts/sync-git-docker-ignore.sh
+++ scripts/sync-git-docker-ignore.sh
@@ -4,7 +4,7 @@
 # I have chosen to use the .gitignore file as the source of truth. But reversal is also possible.
 
 # My idea is to one static path. This path will be point to workspace root.
-# It will search for every project using Angular, React and Django.
+# It will search for every project using Angular, Next and Django.
 # For now, we will only focus on these three. For simplicity.
 
 # 1. Verify the project type and the root folder of the project. If not found, skip the project.
@@ -26,71 +26,77 @@
 # ./sync-git-docker-ignore.sh
 
 # Static path
-WORKSPACE_ROOT=~C:/Users/User/work_space/digital-book
+WORKSPACE_ROOT=../../digital-book
 
 # Function to copy the content of .gitignore file to .dockerignore file
 function copy_git_docker_ignore() {
-    local project_root=$1
-    local git_ignore=$project_root/.gitignore
-    local docker_ignore=$project_root/.dockerignore
+  local project_root=$1
+  local git_ignore=$project_root/.gitignore
+  local docker_ignore=$project_root/.dockerignore
 
-    if [ -f "$docker_ignore" ]; then
-        # Check if the content of the .gitignore file is different from the content of the .dockerignore file
-        diff=$(diff -q "$git_ignore" "$docker_ignore")
-        if [ "$diff" != "" ]; then
-            echo "Updating $docker_ignore with the content of $git_ignore"
-            cp "$git_ignore" "$docker_ignore"
-        else
-            echo "The content of $git_ignore is the same as the content of $docker_ignore. Skipping the project."
-        fi
-    else
-        echo "Creating $docker_ignore and copying the content of $git_ignore"
-        cp "$git_ignore" "$docker_ignore"
-    fi
+  # Check if the .dockerignore file is present in the root folder
+  if [ ! -f "$docker_ignore" ]; then
+    echo "Creating and updating .dockerignore file"
+    cp "$git_ignore" "$docker_ignore"
+    return 0
+  fi
+
+  # Check if the content of the .gitignore file is different from the content of the .dockerignore file
+  diff=$(diff -q "$git_ignore" "$docker_ignore")
+  if [ "$diff" != "" ]; then
+    echo "Updating .dockerignore with the content of .gitignore"
+    cp "$git_ignore" "$docker_ignore"
+    return 0
+  else
+    echo "Skipping, .dockerignore is up to date."
+    return 1
+  fi
 }
 
-# Function to check if the project is using Angular, React or Django
+# Function to check if the project is using Angular, Next or Django
 function check_project_type() {
-    local project_root=$1
+  local project_root=$1
 
-    if [ -f "$project_root/package.json" ]; then
-        # Check if the project is using Angular or React
-        if [ -f "$project_root/angular.json" ] || [ -f "$project_root/next-env.d.ts" ]; then
-            return 0
-        fi
-    elif [ -f "$project_root/manage.py" ]; then
-        # Check if the project is using Django
-        return 0
-    fi
-
-    return 1
+  if [ -f "$project_root/angular.json" ] || [ -f "$project_root/next.config.mjs" ] || [ -f "$project_root/digital_book_backend_django/settings.py" ]; then
+    return 0
+  fi
+  return 1
 }
 
-# Function to check if the project is using Git
-function check_git_usage() {
-    local project_root=$1
+# Function to check if the project is using Git and Docker
+function check_git_docker_usage() {
+  local project_root=$1
 
-    if [ -d "$project_root/.git" ]; then
-        return 0
-    fi
+  if [ -f "$project_root/.gitignore" ] && [ -f "$project_root/Dockerfile" ] ; then
+    return 0
+  fi
 
-    return 1
+  return 1
 }
 
+echo "****************************************************************"
 # Main logic to iterate over projects
 for project in "$WORKSPACE_ROOT"/*; do
-    if [ -d "$project" ]; then
-        check_project_type "$project"
-        # shellcheck disable=SC2181
-        if [ $? -eq 0 ]; then
-            check_git_usage "$project"
-            if [ $? -eq 0 ]; then
-                copy_git_docker_ignore "$project"
-            else
-                echo "Skipping $project: Git not used."
-            fi
-        else
-            echo "Skipping $project: Not an Angular, React, or Django project."
-        fi
+  if [ -d "$project" ]; then
+    echo "================="
+    echo "Checking project: $project"
+    check_project_type "$project"
+    # shellcheck disable=SC2181
+    if [ $? -eq 0 ]; then
+      check_git_docker_usage "$project"
+      if [ $? -eq 0 ]; then
+        copy_git_docker_ignore "$project"
+      else
+        echo "Skipping, Git or Docker is not used."
+      fi
+    else
+      echo "Skipping, Not an Angular, Next, or Django!!!"
     fi
+else
+  echo "Skipping, Not a directory."
+
+  echo "================="
+  fi
 done
+
+echo "****************************************************************"
